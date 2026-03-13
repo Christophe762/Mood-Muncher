7e01736c9fb0c5a9bb4b0d58bb2735f74d96f98e
Christophe762 <64017626+Christophe762@users.noreply.github.com>
2026-03-12 20:23:38 -0400

    chore(terraform): add initial Terraform configuration

---
+++ terraform/variables.tf
@@ -0,0 +1,14 @@
+variable "location" {
+  description = "Azure region"
+  default     = "canadacentral"
+}
+
+variable "resource_group_name" {
+  description = "Main resource group"
+  default     = "mood-muncher-rg"
+}
+
+variable "project_name" {
+  description = "Project prefix"
+  default     = "moodmuncher"
+}
